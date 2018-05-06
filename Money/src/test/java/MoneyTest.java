187093c6dec6e0b8c6e49573786ba69cf41dc911
Terry Yun <nooheat1228@gmail.com>
2018-05-06 23:18:16 +0900

    C12-드디어, 더하기

--- Money/src/test/java/MoneyTest.java
+++ Money/src/test/java/MoneyTest.java
@@ -20,4 +20,12 @@ public class MoneyTest {
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
 }
