ff4fe29760e927daa5e02401ddf74990405c2146
Terry Yun <nooheat1228@gmail.com>
2018-05-06 23:44:41 +0900

    C13-진짜로 만들기

--- Money/src/main/java/Money.java
+++ Money/src/main/java/Money.java
@@ -36,6 +36,11 @@ public class Money implements Expression {
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Money reduce(String to) {
+        return this;
     }
 }
